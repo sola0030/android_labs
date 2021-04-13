56e63d93c447eb8b309be8305ea963501961a7f0
admin <sola0030@algonquinlive.com>
2021-04-13 08:54:19 -0700

    lab1

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- app/src/test/java/com/example/androidlabs/ExampleUnitTest.java
+++ app/src/test/java/com/example/androidlabs/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.androidlabs;
 
 import org.junit.Test;
 
