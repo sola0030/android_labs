16f5094f7642cbe345082f5306a1c6b2b7dd431b
admin <sola0030@algonquinlive.com>
2021-04-13 18:03:51 -0700

    lab8

--- app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
+++ app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
@@ -1,9 +1,8 @@
 package com.example.androidlabs;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,9 +11,11 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +26,8 @@ public class ChatRoomActivity extends AppCompatActivity {
     List<MessageModel> messageModelList2 = new ArrayList<>();
     Button btnSend;
     Button btnReceive;
+    DBAdapter db;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,30 +38,97 @@ public class ChatRoomActivity extends AppCompatActivity {
         editMessage = (EditText)findViewById(R.id.editMessage);
         btnSend = (Button)findViewById(R.id.btnSend);
         btnReceive = (Button)findViewById(R.id.btnReceive);
+        db =  new DBAdapter(this);
+        boolean isTable = findViewById(R.id.fragmentLocation) != null;
+        viewMessage();
+        lv.setOnItemClickListener((list, item, position, id) -> {
+            Bundle dataToPass = new Bundle();
+            dataToPass.putString("item", messageModelList2.get(position).message);
+            dataToPass.putInt("id", position);
+            dataToPass.putLong("db_id", messageModelList2.get(position).messageID);
 
 
-        btnSend.setOnClickListener(c -> {
-            String message = editMessage.getText().toString();
-
-            MessageModel model = new MessageModel(message, true);
-            messageModelList2.add(model);
-            editMessage.setText("");
-            ChatAdapter adt = new ChatAdapter(messageModelList2, getApplicationContext());
-            lv.setAdapter((ListAdapter) adt);
+            if (isTable){
+                DetailFragment dFragment = new DetailFragment(); //add a DetailFragment
+                dFragment.setArguments( dataToPass ); //pass it a bundle for information
+                dFragment.setTablet(true);  //tell the fragment if it's running on a tablet or not
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .add(R.id.fragmentLocation, dFragment) //Add the fragment in FrameLayout
+                        .addToBackStack("AnyName") //make the back button undo the transaction
+                        .commit(); //actually load the fragment.
+            }else {
+                Intent emptyActivity = new Intent(this, EmptyActivity.class);
+                emptyActivity.putExtras(dataToPass);
+                startActivityForResult(emptyActivity, 345);
+            }
 
         });
+/*        btnSend.setOnClickListener(c -> {
+            String message = editMessage.getText().toString();
+//            MessageModel model = new MessageModel(message, true);
+//            messageModelList2.add(model);
+//            ChatAdapter adt = new ChatAdapter(messageModelList2, getApplicationContext());
+//            lv.setAdapter(adt);
+            db.insertMessage(message, true);
+            editMessage.setText("");
+            messageModelList2.clear();
+            viewMessage();
+        });*/
 
         btnReceive.setOnClickListener(c -> {
             String message = editMessage.getText().toString();
-            MessageModel model = new MessageModel(message, false);
-            messageModelList2.add(model);
+//            MessageModel model = new MessageModel(message, false);
+//            messageModelList2.add(model);
+
+//            ChatAdapter adt = new ChatAdapter(messageModelList2, getApplicationContext());
+//            lv.setAdapter(adt);
+            db.insertMessage(message, false);
             editMessage.setText("");
-            ChatAdapter adt = new ChatAdapter(messageModelList2, getApplicationContext());
-            lv.setAdapter(adt);
+            messageModelList2.clear();
+
+            viewMessage();
+
         });
 
         Log.e("ChatRoomActivity","onCreate");
 
+
+    }
+
+    private void viewMessage(){
+        Cursor cursor = db.getAllMessages();
+        //hien so message trong db
+        Log.d("message",String.valueOf(cursor.getCount()));
+
+        if (cursor.getCount() != 0){
+            while (cursor.moveToNext()){
+                MessageModel model = new MessageModel(cursor.getString(1), cursor.getInt(2)==0?true:false, cursor.getLong(0));
+                messageModelList2.add(model);
+                ChatAdapter adt = new ChatAdapter(messageModelList2, getApplicationContext());
+                lv.setAdapter(adt);
+
+            }
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 345) {
+            if (resultCode == RESULT_OK) //if you hit the delete button instead of back button
+            {
+                long id = data.getLongExtra("db_id", 0);
+                deleteMessageId((int) id);
+            }
+        }
+    }
+
+    public void deleteMessageId(int id)
+    {
+
+        db.deleteEntry(id);
+        messageModelList2.clear();
+        viewMessage();
     }
 
 
@@ -67,10 +137,13 @@ public class ChatRoomActivity extends AppCompatActivity {
 class MessageModel {
     public String message;
     public boolean isSend;
+    public long messageID;
 
-    public MessageModel(String message, boolean isSend) {
+
+    public MessageModel(String message, boolean isSend, long messageID) {
         this.message = message;
         this.isSend = isSend;
+        this.messageID = messageID;
     }
 
     public MessageModel() {
@@ -90,6 +163,13 @@ class MessageModel {
 
     public void setSend(boolean send) {
         isSend = send;
+    }
+    public long getMessageID() {
+        return messageID;
+    }
+
+    public void setMessageID(long messageID) {
+        this.messageID = messageID;
     }
 }
 //test folk
@@ -123,16 +203,17 @@ class ChatAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         View view = convertView;
 
-        if (view == null){
-            if (messageModelList.get(position).isSend()){
-                view = inflater.inflate(R.layout.activity_main_send, null);
 
-            }else {
-                view = inflater.inflate(R.layout.activity_main_receive, null);
-            }
-            TextView messageText = (TextView)view.findViewById(R.id.messageText);
-            messageText.setText(messageModelList.get(position).message);
+        if (messageModelList.get(position).isSend()){
+            view = inflater.inflate(R.layout.activity_main_send, null);
+
+        }else {
+            view = inflater.inflate(R.layout.activity_main_receive, null);
         }
+        TextView  messageText = (TextView)view.findViewById(R.id.messageText);
+        messageText.setText(messageModelList.get(position).message);
+
         return view;
     }
 }
+    
